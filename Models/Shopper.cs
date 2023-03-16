794727df427918f97cc801cf82b20ee963f1d322
Mark Pavel <markpavel02@gmail.com>
2023-03-16 15:34:46 -0400

    Restored Shopper Model

--- Models/Shopper.cs
+++ Models/Shopper.cs
@@ -11,7 +11,11 @@ namespace Shamazon.Models
 
         public string Email { get; set; }
 
-        public string Nickname { get; set; }
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string[]? Interests { get; set; }
 
