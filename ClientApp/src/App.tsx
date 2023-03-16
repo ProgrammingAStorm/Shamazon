8cb817d3e72ff3280f3789ebda7a4fc9f383d4bc
Mark Pavel <markpavel02@gmail.com>
2023-03-16 13:37:09 -0400

    Added Basic Routing And Auth For Front-End

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,9 +1,91 @@
-import { useState } from 'react'
+import { useEffect, useRef } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  return (
-    <div className='flex flex-col'>
-      <div>Shamazon</div>
+  return <Routes >
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+
+      <Route path="*" element={<NoMatch />} />
+    </Route>
+  </Routes>;
+}
+
+function Layout() {
+  return <>
+    <Header />
+
+    <Outlet />
+
+    <Footer />
+  </>;
+}
+
+function Header() {
+  const {
+    user,
+    isAuthenticated,
+    loginWithPopup,
+    logout,
+  } = useAuth0();
+
+  const mode = useRef('')
+
+  useEffect(() => {
+    console.log("From header", user)
+
+    if (isAuthenticated) {
+      switch (mode.current) {
+
+      }
+    }
+  }, [user])
+
+  return <header>
+    <h1>Shamazon</h1>
+
+    {!isAuthenticated ? <button onClick={async () => {
+      await loginWithPopup({})
+    }}>
+      Log In
+    </button> : <button onClick={() => logout({})}>
+      Logout
+    </button>}
+
+
+  </header>;
+}
+
+function Home() {
+  return <main>
+    home
+  </main>
+}
+
+function Footer() {
+  return <footer>Made by mark</footer>;
+}
+
+function NoMatch() {
+  return <main>
+    <h1>No Match!</h1>
+
+    <p>
+      Something went wrong and the page you navigated to doesn't exist!
+    </p>
+
+    <h4>
+      Sorry! {';('}
+    </h4>
+  </main>
+}
+
+export default App
+
+{/* <div>Shamazon</div>
+      
       <button onClick={async () => {
         const request = await fetch('/api/test')
         const response = await request.json()
@@ -12,6 +94,7 @@ function App() {
       }}>
         Press to get list of routes.
       </button>
+      
       <button onClick={async () => {
         const request = await fetch('/api/shoppers')
         const response = await request.json()
@@ -19,9 +102,4 @@ function App() {
         console.log(response)
       }}>
         Press to get all shoppers.
-      </button>
-    </div>
-  )
-}
-
-export default App
+      </button> */}
--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App'
+import { getConfig } from "./config";
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider {...providerConfig}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
 )
