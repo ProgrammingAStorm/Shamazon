5cc73f6438190003af44674686725adb774cd8c0
Mark Pavel <markpavel02@gmail.com>
2023-03-23 11:59:55 -0400

    Filled Out Seller Model

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,5 +1,22 @@
-namespace Shamazon.Models {
-    public class Seller {
-        
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shamazon.Models
+{
+    public class Seller
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Product[]? Products { get; set; }
+
+        public Order[]? Orders { get; set; }
     }
 }
