19f22dba8eb28a7d3315a202ff64b4ae4ca00e66
Mark Pavel <markpavel02@gmail.com>
2023-03-28 15:30:36 -0400

    Refactored NoMatch To Be Route Agnostic

---
+++ ClientApp/src/components/NoMatch.tsx
@@ -0,0 +1,13 @@
+export default function NoMatch() {
+    return <main>
+        <h1>No Match!</h1>
+
+        <p>
+            Something went wrong and the page you navigated to doesn't exist!
+        </p>
+
+        <h4>
+            Sorry! {';('}
+        </h4>
+    </main>
+}
