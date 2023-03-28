2b27751458a44b5a4605bc7cecabca1b654376c5
Mark Pavel <markpavel02@gmail.com>
2023-03-28 15:34:59 -0400

    Refactored To Shopper Context

--- ClientApp/src/components/Shopper/Header.tsx
+++ ClientApp/src/components/Shopper/Header.tsx
@@ -3,17 +3,17 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 //Util imports
-import { UserContext } from "../../utils/context";
+import { ShopperContext } from "../../utils/context";
 
 export default function Header() {
     const navigate = useNavigate();
-    const [user, setUser] = useContext(UserContext)
+    const [shopper, setShopper] = useContext(ShopperContext)
 
     return <header>
         <h1 onClick={() => navigate("/")}>Shamazon</h1>
 
         {
-            user.token === ''
+            shopper.token === ''
                 ?
                 <button onClick={() => navigate("/login")}>
                     Log in
@@ -21,7 +21,7 @@ export default function Header() {
                 :
                 <button onClick={() => {
                     localStorage.setItem('token', '')
-                    setUser({ token: '' })
+                    setShopper({ token: '' })
                     navigate('/')
                 }}>
                     Log out
