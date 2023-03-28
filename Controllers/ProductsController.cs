49e4e7fb81d92f85ed86a296eb66e971e6891863
Mark Pavel <markpavel02@gmail.com>
2023-03-28 17:50:31 -0400

    Added ProductsController Boilerplate

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+using Shamazon.Models;
+using Shamazon.Services;
+using Shamazon.Helpers;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shamazon.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    // [HttpPost("upload")]
+    // public async Task<ActionResult<Product>> Upload()
+    // {
+
+    // }
+}
