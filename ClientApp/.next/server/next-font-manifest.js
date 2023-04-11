623e516ce0cb58099387c6491172a4817a5f36d2
Mark Pavel <markpavel02@gmail.com>
2023-04-11 13:27:53 -0400

    Next.JS

---
+++ ClientApp/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST={pages:{},app:{'/mnt/wwn-0x5000c500c757436c-part1/Workspace/Shamazon/ClientApp/node_modules/next/font/google/target.css?{"path":"app/page.tsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/2aaf0723e720e8b9-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
