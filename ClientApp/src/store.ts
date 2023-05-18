976fd116d3926bb0dc3954de2e56937bda97234a
Mark Pavel <markpavel02@gmail.com>
2023-05-18 17:05:29 -0400

    Added User Reducer And Exportation Of Root State And App Dispatch Types

--- ClientApp/src/store.ts
+++ ClientApp/src/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import shopperReducer from './shopperSlice';
+import userReducer from './userSlice';
 
 export const store = configureStore({
     reducer: {
-        shopper: shopperReducer
+        shopper: shopperReducer,
+        user: userReducer
     }
 })
 
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, shoppers: shoppersState}
-// export type AppDispatch = typeof store.dispatch
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, shoppers: shoppersState}
+export type AppDispatch = typeof store.dispatch
