71bd71e559be065d8bebda117fa4b49587bb5cf1
Mark Pavel <markpavel02@gmail.com>
2023-05-18 12:22:35 -0400

    Refactored To Implemented Server Action

---
+++ ClientApp/app/login/page.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+// React imports
+import { ChangeEvent, useState } from "react"
+import Link from "next/link";
+
+//Util imports
+import { validateEmail, validatePassword } from "../../src/validation";
+import { handleLogin } from "./actions";
+
+export default function LogIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [messages, setMessages] = useState({ password: '', email: '' });
+
+    return <main>
+        <form action={handleLogin}>
+            <input
+                type="email"
+                placeholder="Email"
+                className="border-cyan-900 border m-1"
+                name="password"
+                value={email}
+                required
+                onChange={handleEmail}
+            ></input>
+            <input
+                type="password"
+                placeholder="Password"
+                className="border-cyan-900 border m-1"
+                name="password"
+                value={password}
+                required
+                onChange={handlePassword}
+            ></input>
+
+            <button type="submit" className="border-cyan-900 border m-1 p-1">Submit</button>
+
+            {messages.email != '' || messages.password != ''
+                ? <div>
+                    <p>{messages.email}</p>
+                    <p>{messages.password}</p>
+                </div>
+                : <p></p>}
+        </form>
+
+        <Link href="/signup">Create account?</Link>
+    </main>
+
+    function handleEmail(e: ChangeEvent<HTMLInputElement>) {
+        const targetEmail = e.target.value;
+
+        setEmail(targetEmail);
+
+        if(targetEmail === '') {
+            return setMessages({ ...messages, email: '' })
+        }
+
+        if (!validateEmail(targetEmail)) {
+            setMessages({ ...messages, email: 'Email format is incorrect.' })
+        } else {
+            setMessages({ ...messages, email: '' })
+        }
+    }
+
+    function handlePassword(e: ChangeEvent<HTMLInputElement>) {
+        const targetPassword = e.target.value;
+
+        setPassword(targetPassword);
+
+        if(targetPassword === '') {
+            return setMessages({ ...messages, password: '' })
+        }
+
+        if (!validatePassword(targetPassword)) {
+            setMessages({ ...messages, password: 'Password format is incorrect.' })
+        } else {
+            setMessages({ ...messages, password: '' })
+        }
+    }
+}
