531a39fec4c4f4a114ff45862fa7342a6fdbc06b
Mark Pavel <markpavel02@gmail.com>
2023-05-18 13:07:29 -0400

    Refactored To No Longer Include Apollo Provider

--- ClientApp/app/login/layout.tsx
+++ ClientApp/app/login/layout.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { ApolloProvider } from "@apollo/client"
-import client from "@/src/apollo-front"
 
 export default function LoginLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    return <ApolloProvider client={client}>
-        {children}
-    </ApolloProvider>
+    return {children}
 }
