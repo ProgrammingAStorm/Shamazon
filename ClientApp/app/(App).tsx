0b7fc76baa436e2ff9229228fb1278c66f3c39a2
Mark Pavel <markpavel02@gmail.com>
2023-05-18 17:02:28 -0400

    Added App Component To Wrap Main Layout In Redux Provider As Client

---
+++ ClientApp/app/(App).tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from "react";
+import { Provider } from 'react-redux'
+import { store } from '@/src/store'
+
+import Footer from './(Footer)'
+import Header from './(Header)'
+
+export default function App({ children }: { children: React.ReactNode | React.ReactNode[] }) {
+    return <Provider store={store}>
+        <Header />
+        {children}
+        <Footer />
+    </Provider>
+}
