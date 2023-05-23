d9a62d62239eb18b8b6c377d7589b6029579b30e
Mark Pavel <markpavel02@gmail.com>
2023-05-23 17:11:33 -0400

    Refactored To Set Shopper To False When On A Seller Route

--- ClientApp/app/seller/layout.tsx
+++ ClientApp/app/seller/layout.tsx
@@ -2,16 +2,21 @@
 
 import { ReactNode, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { sellerSelector } from "@/src/redux/slices/sellerSlice";
+import { setShopperFalse } from "@/src/redux/slices/userSlice";
 
 export default function SellerLayout({children}: {children: ReactNode}) {
     const seller = useSelector(sellerSelector);
 
-    const router = useRouter()
+    const dispatch = useDispatch();
+
+    const router = useRouter();
 
     useEffect(() => {
+        dispatch(setShopperFalse())
+
         if(!seller.seller) {
             router.push('/seller/login')
         }
