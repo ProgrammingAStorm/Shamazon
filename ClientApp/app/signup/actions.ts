acd800286d2ec05643ff082602f79d46797c74f1
Mark Pavel <markpavel02@gmail.com>
2023-05-23 14:36:42 -0400

    Added Login Action

---
+++ ClientApp/app/signup/actions.ts
@@ -0,0 +1,54 @@
+'use server';
+
+import { gql } from "@apollo/client";
+import client from "@/src/apollo";
+
+const SIGNUP_MUTATION = gql`
+    mutation signup(
+        $email: String!
+        $password: String!
+        $firstName: String!
+        $lastName: String!
+    ) {
+        shoppers {
+            signup(email: $email, password: $password, firstName: $firstName, lastName: $lastName) {
+                token
+                status
+                payload {
+                    id
+                    firstName
+                    lastName
+                    interests
+                    reviews {
+                        __typename
+                    }
+                    orders {
+                        __typename
+                    }
+                    cart {
+                        __typename
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export async function handleSignup(formData: FormData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName')
+
+    const mutation = await client.mutate({
+        mutation: SIGNUP_MUTATION,
+        variables: {
+            email,
+            password,
+            firstName,
+            lastName
+        }
+    });
+
+    return mutation.data.shoppers.signup
+}
