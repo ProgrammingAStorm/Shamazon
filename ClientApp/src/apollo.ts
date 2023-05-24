97ba0daba5960bd84fbc922b53ee3a96a6e5191d
Mark Pavel <markpavel02@gmail.com>
2023-05-24 12:28:44 -0400

    Removed Create Upload Link

--- ClientApp/src/apollo.ts
+++ ClientApp/src/apollo.ts
@@ -1,10 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-const { createUploadLink } = require('apollo-upload-client');
 
 const client = new ApolloClient({
     uri: "http://localhost:5164/graphql",
     cache: new InMemoryCache(),
-    link: createUploadLink()
 });
 
 export default client;
