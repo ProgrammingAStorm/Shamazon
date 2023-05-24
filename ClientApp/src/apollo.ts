bb705bcc549cb39dbdc2a538f0635d66fe58ed3e
Mark Pavel <markpavel02@gmail.com>
2023-05-24 11:01:56 -0400

    Added Upload Link To Apollo Client

--- ClientApp/src/apollo.ts
+++ ClientApp/src/apollo.ts
@@ -1,11 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+const { createUploadLink } = require('apollo-upload-client');
 
 const client = new ApolloClient({
     uri: "http://localhost:5164/graphql",
     cache: new InMemoryCache(),
+    link: createUploadLink()
 });
 
 export default client;
-
-//https://localhost:7088
-//http://localhost:5164
