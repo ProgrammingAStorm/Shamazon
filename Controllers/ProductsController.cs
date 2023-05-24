a41a95d0a2a768f64f6f89778bbe1afff37285ff
Mark Pavel <markpavel02@gmail.com>
2023-05-24 12:27:48 -0400

    Refactored Product Controller To Work With Graphql

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -9,21 +9,21 @@ namespace Shamazon.Controllers;
 public class ProductsController : GraphController
 {
     [Mutation("Upload")]
-    public async Task<IGraphProduct> Upload(Product newProduct)
+    public async Task<IGraphProduct> Upload(string Name, string Description, float Price, List<string> images)
     {
         // TODO enforce validation so that no products with duplicated names are generated
         // TODO enforce validation so that base64 strings are actually base64 strings
 
-        var byteArrays = new List<byte>[newProduct.ImageUrls.ToArray().Length];
+        // var byteArrays = new List<byte>[newProduct.ImageUrls.ToArray().Length];
 
-        foreach (var item in newProduct.ImageUrls)
-        {
-            var splitString = item.Split(',')[1];
+        // foreach (var item in newProduct.ImageUrls)
+        // {
+        //     var splitString = item.Split(',')[1];
 
-            List<byte> decodedString = Convert.FromBase64String(splitString).ToList();
+        //     List<byte> decodedString = Convert.FromBase64String(splitString).ToList();
 
-            byteArrays.Append(decodedString);
-        }
+        //     byteArrays.Append(decodedString);
+        // }
 
         // TODO convert byte arrays into memory streams
         // TODO send memory streams to AWS S3 bucket to be stored 
