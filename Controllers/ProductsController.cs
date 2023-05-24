6854fa799066091ee84e77ac9f048574cad5c347
Mark Pavel <markpavel02@gmail.com>
2023-05-24 15:51:58 -0400

    Added Functionality To Convert Images Strings To Memory Streams

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,18 +14,13 @@ public class ProductsController : GraphController
         // TODO enforce validation so that no products with duplicated names are generated
         // TODO enforce validation so that base64 strings are actually base64 strings
 
-        // var byteArrays = new List<byte>[newProduct.ImageUrls.ToArray().Length];
+        var imageStreams = new List<MemoryStream>();
 
-        // foreach (var item in newProduct.ImageUrls)
-        // {
-        //     var splitString = item.Split(',')[1];
+        foreach (var item in images.ToArray())
+        {
+            imageStreams.Add(Base64ToStream(item));
+        }
 
-        //     List<byte> decodedString = Convert.FromBase64String(splitString).ToList();
-
-        //     byteArrays.Append(decodedString);
-        // }
-
-        // TODO convert byte arrays into memory streams
         // TODO send memory streams to AWS S3 bucket to be stored 
         // TODO replice ImageUrls property in newProduct with actual list of Cloud Flare links of the newly added images
         // TODO use newProduct to generate a new product in the database
