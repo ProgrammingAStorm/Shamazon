302928c4f574cc7774ca275c7d3636c24d458604
Mark Pavel <markpavel02@gmail.com>
2023-06-03 12:32:23 -0400

    Refactored To Set Auth Header To Relevent Token

--- ClientApp/src/apollo.ts
+++ ClientApp/src/apollo.ts
@@ -1,7 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: "http://localhost:5164/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+    const isShopper = localStorage.getItem('isShopper');
+
+    let token;
+
+    if(isShopper) {
+        token = localStorage.getItem('shopperToken');
+    } else {
+        token = localStorage.getItem('sellerToken');
+    }
+
+    return {
+        ...headers,
+        authorization: `Bearer ${token}`
+    }
+})
 
 const client = new ApolloClient({
-    uri: "http://localhost:5164/graphql",
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 });
 
