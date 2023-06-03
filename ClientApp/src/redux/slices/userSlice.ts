7e71dca7738dd864e1bd5c05c58fd98c8599e380
Mark Pavel <markpavel02@gmail.com>
2023-06-03 12:19:21 -0400

    Refactored To Set State Of IsShopper In Local Storage

--- ClientApp/src/redux/slices/userSlice.ts
+++ ClientApp/src/redux/slices/userSlice.ts
@@ -14,12 +14,18 @@ export const userSlice = createSlice({
     initialState,
     reducers: {
         toggleShopper: state => {
+            localStorage.removeItem('isShopper');
+            localStorage.setItem('isShopper', !state.isShopper ? 'true' : 'false')
             state.isShopper = !state.isShopper;
         },
         setShopperTrue: state => {
+            localStorage.removeItem('isShopper');
+            localStorage.setItem('isShopper', 'true')
             state.isShopper = true;
         },
         setShopperFalse: state => {
+            localStorage.removeItem('isShopper');
+            localStorage.setItem('isShopper', 'false')
             state.isShopper = false;
         }
     }
