baa7dc4d03100d9051ce5c5eaaa72ca08618013f
Mark Pavel <markpavel02@gmail.com>
2023-06-03 12:21:43 -0400

    Added Functionality To Set IsShopper In Local Storage To True When The Shopper Page Is Rendered

--- ClientApp/app/page.tsx
+++ ClientApp/app/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 //React imports
+
+import { useEffect } from "react";
 
 //Util imports
 
 //Component imports
 
 export default async function Home() {
+    useEffect(() => {
+        localStorage.removeItem('isShopper');
+        localStorage.setItem('isShopper', 'true');
+    }, [])
+
     return <main>
         home
     </main>;
